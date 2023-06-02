65d04d14287c385f601c69abea4b8158844c3bd7
Santy228335 <132012357+Santy228335@users.noreply.github.com>
2023-06-02 23:03:00 +0300

    Create Home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,52 @@
+const Test = () => {
+    return (
+        <div className="app-wrapper">
+            <header className='header'>
+                <img src="https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr" />
+            </header>
+            <nav className='nav'>
+                <div>
+                   <a>Profile</a>
+                </div>
+                <div>
+                   <a>Messages</a>
+                </div>
+                <div>
+                   <a>News</a>
+                </div>
+                <div>
+                   <a>Music</a>
+                </div>
+                <div>
+                   <a>Settings</a>
+                </div>
+            </nav>
+            <div class='content'>
+                <div>
+                <img src="https://hips.hearstapps.com/hmg-prod/images/champagne-beach-espiritu-santo-island-vanuatu-royalty-free-image-1655672510.jpg?crop=1.00xw:0.755xh;0,0.173xh&resize=1200:*" />
+                </div>
+                <div>
+                    ava + desc
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            Post 1
+                        </div>
+                        <div>
+                            Post 2
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            </div>
+        
+    );
+}
+
+export default Test;
